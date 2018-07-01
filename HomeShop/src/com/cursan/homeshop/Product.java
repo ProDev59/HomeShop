97933925449efec41e7f9b9e7ed553a3e01ec276
DKludo59 <ludovicplatevoet@free.fr>
2018-07-01 15:21:08 +0200

    Création du projet HomeShop -la Classe Bill (facture) -la Classe Customer (client) -la Classe Product (produit)

---
+++ HomeShop/src/com/cursan/homeshop/Product.java
@@ -0,0 +1,44 @@
+package com.cursan.homeshop;
+
+public class Product {
+
+    String name;
+    String description;
+    double price;
+
+
+
+    /**
+     * Affiche description complète du produit
+     */
+    public void look(){
+
+        System.out.println(String.format(name + " : " + price + "%n" + description));
+
+    }
+
+    /**
+     * Ajouter le produit a la facture
+     * @param bill la facture concerné
+     * @param quantity la quantité du produit concerné
+     */
+    public void buy(Bill bill, Integer quantity){
+
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+}
