d0d06f5ec67caa7fef4985a0663beb988c755b93
DKludo59 <ludovicplatevoet@free.fr>
2018-07-01 15:56:00 +0200

    Création des constructeurs du projet HomeShop

--- HomeShop/src/com/cursan/homeshop/Bill.java
+++ HomeShop/src/com/cursan/homeshop/Bill.java
@@ -1,4 +1,35 @@
 package com.cursan.homeshop;
 
+import java.util.Map;
+
 public class Bill {
+
+    private Customer customer;
+    private Map<Product, Integer> products;
+
+
+    //Contructeur
+    public Bill(Customer customer){
+        this.customer = customer;
+    }
+
+    /**
+     * Ajouter un produit a la liste products
+     * @param product le produit a ajouter
+     * @param quantity la quantité associer
+     */
+    public void addProduct(Product product, Integer quantity){
+        this.products.put(product, quantity);
+    }
+
+    public Customer getCustomer() {
+        return customer;
+    }
+
+    public Map<Product, Integer> getProducts() {
+        return products;
+    }
+
+
+
 }
--- HomeShop/src/com/cursan/homeshop/Product.java
+++ HomeShop/src/com/cursan/homeshop/Product.java
@@ -6,7 +6,13 @@ public class Product {
     String description;
     double price;
 
+    //Constructeur
 
+    public Product(String name, String description, double price){
+        this.name = name;
+        this.description = description;
+        this.price = price;
+    }
 
     /**
      * Affiche description complète du produit
